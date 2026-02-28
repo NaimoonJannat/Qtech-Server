be7949e64851d740bd8334993f1b5fd9f850b077
NaimoonJannat <naimoonjannat@gmail.com>
2026-02-28 20:59:57 +0600

    port changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 
 // middleware 
